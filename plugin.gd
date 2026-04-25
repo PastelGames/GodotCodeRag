01721fb638d0a11acd3f2579ccf0c70841ff97c5
PastelGames <43424070+PastelGames@users.noreply.github.com>
2026-04-25 16:42:32 -0400

    Fix Dock Loading

--- plugin.gd
+++ plugin.gd
@@ -2,17 +2,13 @@
 class_name GodotRAG
 extends EditorPlugin
 
-var _dock: EditorDock
 var _rag_explorer: Control
 
 func _enter_tree() -> void:
-	_rag_explorer = preload("res://addons/godot_code_rag/rag_explorer.gd").new()
-	_dock = EditorDock.new()
-	_dock.add_child(_rag_explorer)
-	_dock.title = "RAG Explorer"
-	add_dock(_dock)
+	_rag_explorer = preload("res://rag_explorer.gd").new()
+	add_dock(_rag_explorer, PD_3DLIGHT)
 
 func _exit_tree() -> void:
-	if is_instance_valid(_dock):
-		remove_dock(_dock)
-		_dock.queue_free()
+	if is_instance_valid(_rag_explorer):
+		remove_dock(_rag_explorer)
+		_rag_explorer.queue_free()
